6772e9fa8bb70db8ae9d74a139230263e02bd313
cesarjlando-afk <cesarjlando@gmail.com>
2025-12-30 13:52:43 -0300

    service worker

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'sou-brigadista-v1';
+const urlsToCache = [
+  './', // Página principal
+  './index.html' // Seu arquivo HTML
+];
+
+// Instala e armazena em cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Intercepta requisições e serve do cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Atualiza cache quando há nova versão
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
